8941dbafaa409220ffb4b02204b7aa0c286b1536
Burt Beckwith <burt@burtbeckwith.com>
2016-01-29 02:58:58 -0500

    initial commit

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,5 @@
+import test.ThingHelper
+
+beans = {
+	thingHelper(ThingHelper)
+}
---
+++ grails-app/init/mongo_broken_sessions/Application.groovy
@@ -0,0 +1,10 @@
+package mongo_broken_sessions
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
